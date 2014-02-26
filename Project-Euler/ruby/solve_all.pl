bce5f25f8d52e21b5e3c2675055cb6cc8c9ff70c
Nathan McCallum <Vasari@users.noreply.github.com>
2014-02-27 09:11:58 +1000

    More playing around with my Perl stuff

--- Project-Euler/ruby/solve_all.pl
+++ Project-Euler/ruby/solve_all.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/env perl
 
-## I can't be fucked to figure out how to do this in Ruby. Maybe later.
+# I can't be fucked to figure out how to do this in Ruby. Maybe later.
 
 use strict;
 use warnings;
-use v5.10;
+use v5.16;
+
+$|++;
 
 my @files = <???.rb>;
-for (@files) {
-	system "ruby $_";
-}
+system "ruby $_" for (@files);
